578142b116438f1c38bea33b6a59df5de7d11d8a
Akhil Raju <akhildraju@gmail.com>
2020-08-21 10:44:55 -0700

    Sprint Challenge

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,31 @@
 
 ## Exercise I
 
-a)
+a) O(n^3)
 
 
-b)
+b) n * (n/2 + n/4 + n/8 ....)  = O(nLogn)
 
 
-c)
+c) The bunnyEars gets executed based on the value of bunnies with simple recursion. So the answer is O(n)
 
 ## Exercise II
 
+#start from  top floor and throw an egg off each floor until  you  find the one that breaks the egg.
 
+def is_broken(f):
+
+def throw_egg():
+
+max_floor = n
+current_floor = max_floor
+num_eggs = 0
+
+while current_floor >= 0:
+    throw_egg()
+    if is_broken():
+        return num_eggs, (max_floor-current_floor)
+    num_eggs += 1
+    current_floor -= 1
+
+The runtime complexity of the solution is O(n)
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -92,18 +92,65 @@ class SortingRobot:
         """
         return self._light == "ON"
 
+    # def print(self):
+    #     print("list: ", self._list)
+    #     print("position: ", self._position)
+    #     print("item: ", self._item)
+    #     print("item: ", self._light)
+    #     print("----------------------------------")
+    #     pass
+
+
     def sort(self):
-        """
-        Sort the robot's list.
-        """
-        # Fill this out
-        pass
+
+        # Use  the light as a binary variable to see if we need to do further sorting
+        # Initially the robot holds None. Only  way to pick up the first item is to swap first
+        # Go right and swap all entries that needed to be swapped
+        # Once you reach end, go left and swap all entries that needed be swapped
+        # Do this until the light is not ON anymore - nothing to swap
+        # In the end the Robot holds the last item. The only way to drop it off is to swap.
+
+
+        self.set_light_on()
+
+        while self.light_is_on():
+
+            # self.print()
+
+            self.set_light_off()
+
+            while self.can_move_right():
+
+                # self.print()
+                self.swap_item()    
+
+                self.move_right()
+
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+
+            self.swap_item()
+
+            while self.can_move_left():
+                # self.print()
+
+                self.swap_item()
+                self.move_left()
+
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    self.set_light_on()
+
+            # self.print()
+            self.swap_item()
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
+    # l = [15, 41, 58, 49, 26, 4]
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
